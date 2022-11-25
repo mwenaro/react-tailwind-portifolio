1b6bb87a642b7fcec5039da276551b16cee65e45
Mwero Abdalla <mwerothewebmaker@gmail.com>
2022-11-25 16:43:15 +0300

    Added the components  dir

---
+++ src/components/layout/index.js
@@ -0,0 +1,8 @@
+export {default as Home} from  './Home';
+export {default as Contact} from  './Contact';
+export {default as About} from  './About';
+export {default as Skills} from  './Skills';
+export {default as Portifolio} from  './Portifolio';
+export {default as SocialLinks} from  './SocialLinks';
+export {default as NavBar} from  './NavBar';
+export {default as PageWrapper} from  './PageWrapper';
