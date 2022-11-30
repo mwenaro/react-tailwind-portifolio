46a1daa36f5efbb498f0c7cd8ec1777762b3cfd0
Mwero Abdalla <mwerothewebmaker@gmail.com>
2022-11-30 12:44:02 +0300

    Worked on mobile responsiveness and scroll offset

--- src/components/modules/cards/SectionTitleCard.js
+++ src/components/modules/cards/SectionTitleCard.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 
 function SectionTitleCard({ title, subTitle }) {
   return (
-    <div className="py-4 my-5 flex flex-col items-center justify-center ">
+    <div className="py-4 my-5 flex flex-col items-center justify-center pt-14">
       <h1 className="text-3xl md:text-4xl font-bold inline border-b-4 border-gray-500 text-center text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
         {title}
       </h1>
