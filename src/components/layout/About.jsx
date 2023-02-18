ce48d2390923c37fe6a51c27ba0d9aaa5225a53f
Mwero Abdalla <mwerothewebmaker@gmail.com>
2023-02-18 06:46:49 +0300

    updated the about page. addded bio

--- src/components/layout/About.jsx
+++ src/components/layout/About.jsx
@@ -9,7 +9,11 @@ const About = () => {
 
         <div>
           <p className="text-base md:text-lg font-medium">
-My name is Mwero Abdalla. A full-stack developer with 1.5+ years of experience.
+My name is Mwero Abdalla. I am a full-stack developer with 3+ years of experience creating web applications using React, TypeScript, Tailwind, Redux-Toolkit, PHP, MySQL, MongoDB, and Node.js. With a strong background in responsive design and user experience, I am passionate about developing performant and visually appealing web applications.
+
+In my previous roles, I have developed and maintained various e-commerce websites, launched SaaS platforms, and collaborated with designers and project managers to deliver high-quality web applications. I am well-versed in utilizing Redux-Toolkit for state management, writing unit tests using Jest, and integrating the front-end with back-end development using APIs.
+
+In my next role, I am looking to join a dynamic team that values collaboration, innovation, and personal growth. As a full-stack developer, I am eager to contribute to the development of new web applications, work on complex problems, and stay up-to-date with the latest web development trends. My strong skills in React, TypeScript, Tailwind, PHP, MySQL, MongoDB, Node.js, and responsive design make me a valuable addition to any team.
 
 
           </p>
