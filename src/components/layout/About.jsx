fa2b00365c8f6a30951fd273fe6a1e7d0988cff8
Mwero Abdalla <mwerothewebmaker@gmai.com>
2023-03-01 01:41:55 +0300

    task: Update the about page, putting the text into paragraphs

--- src/components/layout/About.jsx
+++ src/components/layout/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
         <SectionTitleCard title={"About"} />
 
         <div className="mx-auto w-fit">
-          <p className="text-base md:text-lg font-medium max-w-ld p-3 my-2">
+          <p className="text-base md:text-lg font-semibold max-w-ld p-3 my-2">
             My name is Mwero Abdalla. I am a full-stack developer with 3+ years
             of experience creating web applications using React, TypeScript,
             Tailwind, Redux-Toolkit, PHP, MySQL, MongoDB, and Node.js. With a
@@ -16,7 +16,7 @@ const About = () => {
             passionate about developing performant and visually appealing web
             applications.
           </p>
-          <p className="text-base md:text-lg font-medium max-w-ld p-3 my-2">
+          <p className="text-base md:text-lg font-semibold max-w-ld p-3 my-2">
             In my previous roles, I have developed and maintained various
             e-commerce websites, launched SaaS platforms, and collaborated with
             designers and project managers to deliver high-quality web
@@ -24,7 +24,7 @@ const About = () => {
             management, writing unit tests using Jest, and integrating the
             front-end with back-end development using APIs.
           </p>
-          <p className="text-base md:text-lg font-medium max-w-ld p-3 my-2">
+          <p className="text-base md:text-lg font-semibold max-w-ld p-3 my-2">
             In my next role, I am looking to join a dynamic team that values
             collaboration, innovation, and personal growth. As a full-stack
             developer, I am eager to contribute to the development of new web
